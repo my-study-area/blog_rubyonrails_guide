d12586c8bcbef91940d795794bbe2582f667ac08
Adriano Avelino <adriano-st777@hotmail.com>
2019-02-22 17:06:04 -0300

    adds fix to show error on creating comments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,10 +5,13 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+    @article.comments.build
+    @comment = Comment.where(article_id: params[:id])
   end
 
   def new
     @article = Article.new
+    @comment = @article.comments.build
   end
 
   def edit
@@ -44,5 +47,9 @@ class ArticlesController < ApplicationController
   private
   def article_params
     params.require(:article).permit(:title, :text)
+  end
+
+  def comment_params
+    params.require(:comment).permit(:commenter, :body)
   end
 end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,14 @@
 class CommentsController < ApplicationController
   def create
     @article = Article.find(params[:article_id])
-    @comment = @article.comments.create(comment_params)
-    redirect_to article_path(@article)
+    @comment = @article.comments.build(comment_params)
+    if(@comment.valid?)
+      # @article.errors = @comment.errors
+      @comment.save
+      redirect_to article_path(@article)
+    else
+      render 'articles/show'
+    end
   end
 
   def destroy
@@ -14,6 +20,6 @@ class CommentsController < ApplicationController
 
   private
   def comment_params
-    params.require(:comment).permit(:commenter, :body)
+    params.require(:comment).permit(:commenter, :body, :article_id)
   end
 end
