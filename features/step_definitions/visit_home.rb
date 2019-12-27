07bbfe1a39030f9b8bde449f6eb181dc971fbf1b
Adriano Avelino <adriano-st777@hotmail.com>
2019-12-26 23:16:56 -0300

    fixes test to visit home

--- features/step_definitions/visit_home.rb
+++ features/step_definitions/visit_home.rb
@@ -1,9 +1,8 @@
 Dado("que eu acesso a página inicial") do
-  visit('http://localhost:3000/')
+  visit root_path
 end
 
 Entao("eu acesso o conteúdo da página") do
-  expect(page).to have_current_path( 'http://localhost:3000/', url: true)
   title = find('h1')
   expect(title.text).to include("Hello World!")
 end
