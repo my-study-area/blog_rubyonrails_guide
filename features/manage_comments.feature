9e7f7ab3960272d1a3a5d7efe45575c9f658f4db
Adriano Avelino <adriano-st777@hotmail.com>
2020-01-09 22:29:30 -0300

    adds test to adding a new comment

--- features/manage_comments.feature
+++ features/manage_comments.feature
@@ -7,3 +7,9 @@ Feature: Manage comments
     Given I have 3 comments in an article
     When I visit comments page from this article
     Then I should see the list of comments this article
+
+  Scenario: Adding a new comment
+    Given I have an article
+    And I visit this article page
+    When I submit a new comment
+    Then I should see the comment in this article
--- features/step_definitions/manage_comments.rb
+++ features/step_definitions/manage_comments.rb
@@ -17,3 +17,27 @@ Then("I should see the list of comments this article") do
   expect(page).to have_content @comments.last.commenter
   expect(page).to have_content @comments.last.body
 end
+
+Given("I have an article") do
+  @title = FFaker::CheesyLingo.title
+  expect{create(:article, title: @title)}.to change {Article.count}.by 1
+end
+
+Given("I visit this article page") do
+  @article = create(:article)
+  visit article_path(@article)
+  expect(page).to have_content "Comments"
+end
+
+When("I submit a new comment") do
+  @commenter = FFaker::NameBR.name
+  @body = FFaker::CheesyLingo.sentence
+  fill_in "comment_commenter", with: @commenter
+  fill_in "comment_body", with: @body
+  click_button "Create Comment"
+end
+
+Then("I should see the comment in this article") do
+  expect(page).to have_content @commenter
+  expect(page).to have_content @body
+end
