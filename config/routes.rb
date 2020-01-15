22a4719804d1d83c2e89a0ea18b8140691b2f3be
Adriano Avelino <adriano-st777@hotmail.com>
2020-01-14 22:39:24 -0300

    adds devise configuration

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
     resources :comments
   end
 
-  root 'welcome#index'
+  devise_for :users
+  root 'articles#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
