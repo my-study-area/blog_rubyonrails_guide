55e1fa1da46a43a8355681e4abe55b541e4e34d3
Adriano Avelino <adriano-st777@hotmail.com>
2020-01-27 22:17:40 -0300

    removes old tests

--- features/step_definitions/visit_article.rb
+++
@@ -1,8 +0,0 @@
-Quando("eu clico no link {string}") do |link_text|
-  find_link(link_text).click
-end
-
-Entao("eu acesso a página de Artigos") do
-  title = find('h1')
-  expect(title.text).to include('Listing Articles')
-end
--- features/step_definitions/visit_home.rb
+++
@@ -1,8 +0,0 @@
-Dado("que eu acesso a página inicial") do
-  visit root_path
-end
-
-Entao("eu acesso o conteúdo da página") do
-  title = find('h1')
-  expect(title.text).to include("Hello World!")
-end
